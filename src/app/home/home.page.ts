1053fca928eae51f184d66df825432d03c6c848c
Danilo Pereira <dnilo@live.com>
2021-08-02 22:14:42 -0300

    Array de pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,72 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  // Criamos um array de pokemons
+  // [] => representa um array (lista)
+  // {} => represneta um objetivo (item)
+  listaPokemons = [
+    {
+      numero: '001',
+      nome: 'Bulbasaur',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+      tipos: [ 'Grass', 'Poison' ]
+    },
+    {
+      numero: '002',
+      nome: 'Ivysaur',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+      tipos: [ 'Grass', 'Poison' ]
+    },
+    {
+      numero: '003',
+      nome: 'Venusaur',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+      tipos: [ 'Grass', 'Poison' ]
+    },
+    {
+      numero: '004',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
+      tipos: [ 'Fire' ]
+    },
+    {
+      numero: '005',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png',
+      tipos: [ 'Fire' ]
+    },
+    {
+      numero: '006',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png',
+      tipos: [ 'Fire' ]
+    },
+    {
+      numero: '007',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png',
+      tipos: [ 'Fire' ]
+    },
+    {
+      numero: '008',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png',
+      tipos: [ 'Fire' ]
+    },
+    {
+      numero: '009',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png',
+      tipos: [ 'Fire' ]
+    },
+    {
+      numero: '010',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png',
+      tipos: [ 'Fire' ]
+    }
+  ];
+
   constructor() {}
 
 }
