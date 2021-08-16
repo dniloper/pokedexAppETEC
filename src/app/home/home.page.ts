7b8d817fb9e761bb35e0eaaf6c5ad9c802209a90
Danilo Pereira <dnilo@live.com>
2021-08-15 21:36:33 -0300

    pagina pokemon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,61 +16,61 @@ export class HomePage {
       numero: '001',
       nome: 'Bulbasaur',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
-      tipos: [ 'Grass', 'Poison' ]
+      tipos: ['Grass', 'Poison']
     },
     {
       numero: '002',
       nome: 'Ivysaur',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
-      tipos: [ 'Grass', 'Poison' ]
+      tipos: ['Grass', 'Poison']
     },
     {
       numero: '003',
       nome: 'Venusaur',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
-      tipos: [ 'Grass', 'Poison' ]
+      tipos: ['Grass', 'Poison']
     },
     {
       numero: '004',
       nome: 'Charmander',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
-      tipos: [ 'Fire' ]
+      tipos: ['Fire']
     },
     {
       numero: '005',
       nome: 'Charmeleon',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png',
-      tipos: [ 'Fire' ]
+      tipos: ['Fire']
     },
     {
       numero: '006',
       nome: 'Charizard',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png',
-      tipos: [ 'Fire', 'Flying' ]
+      tipos: ['Fire', 'Flying']
     },
     {
       numero: '007',
       nome: 'Squirtle',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png',
-      tipos: [ 'Water' ]
+      tipos: ['Water']
     },
     {
       numero: '008',
       nome: 'Wartortle',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png',
-      tipos: [ 'Water' ]
+      tipos: ['Water']
     },
     {
       numero: '009',
       nome: 'Blastoise',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png',
-      tipos: [ 'Water' ]
+      tipos: ['Water']
     },
     {
       numero: '010',
       nome: 'Caterpie',
       foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png',
-      tipos: [ 'Bug' ]
+      tipos: ['Bug']
     }
   ];
 
@@ -89,7 +89,7 @@ export class HomePage {
     const busca: string = evento.target.value;
 
     // Pega o valor digitado no campo de busca
-    if(busca && busca.trim() !== ''){ // Testa se tem alguma coisa no campo
+    if (busca && busca.trim() !== '') { // Testa se tem alguma coisa no campo
       this.listaPokemonsFiltrada = this.listaPokemons.filter(pokemon =>
         pokemon.nome.toLowerCase().includes(busca.trim().toLowerCase())
       );
--- src/app/pokemons/bulbasaur/bulbasaur.page.ts
+++ src/app/pokemons/bulbasaur/bulbasaur.page.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BulbasaurPage implements OnInit {
 
+  pokemon = {
+    numero: '001',
+    nome: 'Bulbasaur',
+    foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+    tipos: ['Grass', 'Poison']
+  };
+
   constructor() { }
 
   ngOnInit() {
